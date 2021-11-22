4f981d516092e9f6ee6e261b3da54bbb10e2aa18
Corinne <83316799+nightsandwich@users.noreply.github.com>
2021-11-22 05:59:11 -0500

    heroku config

--- client/components/Map/TripMap.js
+++ client/components/Map/TripMap.js
@@ -32,7 +32,7 @@ const tripZoom = 12;
 
 export default function TripMap ({tripId}) {
     const { isLoaded, loadError } = useLoadScript({
-        googleMapsApiKey: process.env.API_KEY
+        googleMapsApiKey: API_KEY
     });
 
     const auth = useSelector(state => state.auth);
