00745321af5eb40b490fedbd27c2a326ee858fb2
jmartin-code <jmmartinez331@gmail.com>
2021-12-07 20:15:36 -0500

    modified some styles

--- client/components/Settings/AuthAvatar.js
+++ client/components/Settings/AuthAvatar.js
@@ -12,9 +12,10 @@ import FormControl from '@mui/material/FormControl';
 import Snackbar from '@mui/material/Snackbar';
 import SettingsIcon from '@mui/icons-material/Settings';
 import Alert from '@mui/material/Alert';
+import { Typography } from "@mui/material";
 
 class AuthAvatar extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.state = {
       avatar: '',
@@ -25,32 +26,32 @@ class AuthAvatar extends Component {
     this.handleClose = this.handleClose.bind(this)
   }
 
-  async save (ev) {
+  async save(ev) {
     const { setOpen } = this
     ev.preventDefault()
     const { avatar } = this.state
     const { auth } = this.props
     const { updateUser, me } = this.props
     try {
-      await updateUser({...auth, avatar: avatar });
+      await updateUser({ ...auth, avatar: avatar });
       setOpen()
       await me(auth)
-      this.setState({...this.state, avatar: ''})
+      this.setState({ ...this.state, avatar: '' })
     }
     catch (error) {
       console.log(error)
     }
   }
 
-  setOpen () {
-    this.setState({...this.state, open: true})
+  setOpen() {
+    this.setState({ ...this.state, open: true })
   }
 
-  handleClose () {
-    this.setState({...this.state, open: false})
+  handleClose() {
+    this.setState({ ...this.state, open: false })
   }
-  
-  componentDidMount () {
+
+  componentDidMount() {
     this.el.addEventListener('change', (ev) => {
       const file = ev.target.files[0]
       const reader = new FileReader()
@@ -62,11 +63,11 @@ class AuthAvatar extends Component {
 
     let fileUpload = document.getElementById('fileUpload')
     let fileSizeLimit = document.getElementById('fileSizeLimit')
-    
+
     fileUpload.addEventListener('change', () => {
       let files = fileUpload.files
       if (files.length > 0) {
-        if (files[0].size > 5*1024*1024) {
+        if (files[0].size > 5 * 1024 * 1024) {
           fileSizeLimit.innerText = 'File size cannot exceed 5MB'
           return
         }
@@ -75,64 +76,69 @@ class AuthAvatar extends Component {
     })
   }
 
-  
 
-  render () {
+
+  render() {
     const { avatar, open } = this.state
     const { auth } = this.props
     const { save, handleClose } = this
     let fileSizeLimit = document.getElementById('fileSizeLimit')
-    
+
     return (
-      <Container component="main" maxWidth="xs">
-      <Button component={Link} to='/settings' variant='outlined' color='info' startIcon={<SettingsIcon />}>
-        Back
-      </Button>
-      <Avatar sx={{ height: 60, width: 60, m: 1, bgcolor: 'primary.main'}} src={auth.avatar} >
-        {auth.firstName[0]+auth.lastName[0]}
-      </Avatar>
-      {
-        !!avatar && !fileSizeLimit.innerText && <Avatar sx={{ height: 60, width: 60, m: 1, bgcolor: 'primary.main'}} src={avatar} />
-      }
-      <Snackbar open={open} autoHideDuration={2000} onClose={handleClose} anchorOrigin={{ vertical: 'top', horizontal: 'center' }}>
-      <Alert onClose={handleClose} severity='success' sx={{ width: '100%' }}>
-      Avatar Changed!
-      </Alert>
-      </Snackbar>
-      <Box component="form" onSubmit={save} sx={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-        <Grid container spacing={1} >
-        <Grid item xs={12} >
-          <FormControl variant="outlined">
-            <Button
-                variant="contained"
-                component="label"
+      <Box sx={{ p: 3 }}>
+        <Typography variant='h6' align='center'>
+          Update Profile Image
+        </Typography>
+        <Button component={Link} to='/settings' variant='outlined' color='info' startIcon={<SettingsIcon />}>
+          Back
+        </Button>
+        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <Avatar sx={{ height: 100, width: 100, mt: 3 }} src={auth.avatar} >
+            {auth.firstName[0] + auth.lastName[0]}
+          </Avatar>
+          {
+            !!avatar && !fileSizeLimit.innerText && <Avatar sx={{ height: 60, width: 60, m: 1, bgcolor: 'primary.main' }} src={avatar} />
+          }
+          <Snackbar open={open} autoHideDuration={2000} onClose={handleClose} anchorOrigin={{ vertical: 'top', horizontal: 'center' }}>
+            <Alert onClose={handleClose} severity='success' sx={{ width: '100%' }}>
+              Avatar Changed!
+            </Alert>
+          </Snackbar>
+          <Box component="form" onSubmit={save} sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+            <Grid container >
+              <Grid item xs={12} >
+                <FormControl variant="outlined" sx={{ width: '100%' }}>
+                  <Button
+                    variant="outlined"
+                    component="label"
+                    sx={{ mt: 1 }}
+                    fullWidth
+                  >
+                    Upload File
+                    <input
+                      id='fileUpload'
+                      type="file"
+                      hidden
+                      ref={el => this.el = el}
+                      accept="image/*"
+                    />
+                  </Button>
+                  <span id='fileSizeLimit'></span>
+                </FormControl>
+              </Grid>
+              <Button
+                type="save"
+                variant="outlined"
+                disabled={!avatar || !!fileSizeLimit.innerText}
                 sx={{ mt: 2, mb: 2 }}
-                xs={12}
+                fullWidth
               >
-                Upload File
-                <input
-                  id='fileUpload'
-                  type="file"
-                  hidden
-                  ref={ el => this.el = el}
-                  accept="image/*"
-                />
-              </Button>
-              <span id='fileSizeLimit'></span>
-              </FormControl>
-            </Grid>
-            <Button
-              type="save"
-              variant="contained"
-              disabled={ !avatar || !!fileSizeLimit.innerText }
-              sx={{ mt: 2, mb: 2 }}
-              xs={12}
-            >
                 Save
-            </Button>
-        </Grid>
+              </Button>
+            </Grid>
+          </Box>
+        </Box>
       </Box>
-      </Container>
     )
 
   }
@@ -147,8 +153,8 @@ const mapState = state => {
 const mapDispatch = (dispatch) => {
   return {
     updateUser: (user) => {
-          dispatch(updateUser(user))
-      },
+      dispatch(updateUser(user))
+    },
     me: (auth) => {
       dispatch(me(auth))
     },
