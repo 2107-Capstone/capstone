ea48634d5afd3cbb6a781a24d0184d793a7dde7b
jmartin-code <jmmartinez331@gmail.com>
2021-12-08 22:20:24 -0500

    home page layout

--- client/components/Home/Home.js
+++ client/components/Home/Home.js
@@ -1,14 +1,47 @@
 import React from 'react'
 
 ///////////////////// UI ///////////////////////
-import { Typography } from '@mui/material'
+import { Box, Grid, Typography } from '@mui/material'
 
 const Home = () => {
   return (
-    <div>
-      <Typography align='left'>Home page for logged in and non-logged in users</Typography>
-
-    </div>
+    <Grid container>
+      <Grid item xs={12}>
+        <Box sx={{ height: '50vh', bgcolor: 'lightblue' }}>
+          <Typography>
+            Container 1
+          </Typography>
+        </Box>
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <Box sx={{ height: '40vh', bgcolor: 'pink' }}>
+          <Typography>
+            Container 2
+          </Typography>
+        </Box>
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <Box sx={{ height: '40vh', bgcolor: 'orange' }}>
+          <Typography>
+            Container 3
+          </Typography>
+        </Box>
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <Box sx={{ height: '40vh', bgcolor: 'yellow' }}>
+          <Typography>
+            Container 4
+          </Typography>
+        </Box>
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <Box sx={{ height: '40vh', bgcolor: 'lightgreen' }}>
+          <Typography>
+            Container 5
+          </Typography>
+        </Box>
+      </Grid>
+    </Grid>
   )
 }
 
