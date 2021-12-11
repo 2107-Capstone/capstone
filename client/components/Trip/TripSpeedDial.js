2730e7033171addb2bd3f57bdd7f790429757b64
Corinne <83316799+nightsandwich@users.noreply.github.com>
2021-12-11 05:06:11 -0500

    forgot to add tripspeeddial

---
+++ client/components/Trip/TripSpeedDial.js
@@ -0,0 +1,69 @@
+import React from 'react'
+
+/////////////// MUI /////////////////
+import SpeedDial from '@mui/material/SpeedDial';
+import SpeedDialAction from '@mui/material/SpeedDialAction';
+import Tooltip from '@mui/material/Tooltip'
+import Typography from '@mui/material/Typography'
+
+/////////////// ICONS /////////////////
+import EventIcon from '@mui/icons-material/Event';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import PaidIcon from '@mui/icons-material/Paid';
+import SpeedDialIcon from '@mui/material/SpeedDialIcon';
+
+const TripSpeedDial = ({handleCloseMenu, setOpen, setForm}) => {
+    return (
+        <div>
+            <SpeedDial
+                ariaLabel='Trip SpeedDial'
+                direction='left'
+                FabProps={{variant: 'extended',
+                color: 'primary'}}
+                icon={<SpeedDialIcon />}
+            >
+                <SpeedDialAction
+                    enterTouchDelay={200}
+                    leaveTouchDelay={900}
+                    icon={<PersonAddIcon color='secondary'/>}
+                    tooltipTitle='Invite Friend'
+                    // tooltipOpen
+                    onClick={() => {
+                        handleCloseMenu();
+                        setOpen(true);
+                        setForm('invitefriend')
+                    }}
+                />
+                <SpeedDialAction
+                    enterTouchDelay={200}
+                    leaveTouchDelay={900}
+                    icon={<PaidIcon color='secondary'/>}
+                    tooltipTitle='Add Expense'
+                    // tooltipOpen
+                    // tooltipPlacement='right'
+                    onClick={() => {
+                        handleCloseMenu();
+                        setOpen(true);
+                        setForm('expense')
+                    }}
+                />
+                <SpeedDialAction
+                    enterTouchDelay={200}
+                    leaveTouchDelay={900}
+                    icon={<EventIcon color='secondary'/>}
+                    tooltipTitle='Add Event'
+                    TooltipClasses={{
+                        enterTouchDelay: 300
+                    }}
+                    // tooltipOpen
+                    onClick={() => {
+                        handleCloseMenu();
+                        setOpen(true);
+                        setForm('event')
+                    }}
+                />
+            </SpeedDial>
+        </div>
+    )
+}
+export default TripSpeedDial;
