f92bfb0bcc23de9146bea1a1ed88bea5cd66540e
jmartin-code <jmmartinez331@gmail.com>
2021-12-13 12:45:10 -0500

    added background and animation for the home page

--- client/components/Home/Home.js
+++ client/components/Home/Home.js
@@ -1,17 +1,33 @@
 import React, { createRef, Fragment, useEffect, useRef, useState } from 'react'
 
 ///////////////////// UI ///////////////////////
-import { Box, Collapse, Fade, Zoom, Grid, Grow, IconButton, Slide, Typography, Divider, Button, Container } from '@mui/material'
+import { Box, Grid, Grow, IconButton, Slide, Typography, Divider, Button, Zoom, Collapse, Fade } from '@mui/material'
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 /////////////// Data ////////////
 import data from './data'
 import { Link } from 'react-router-dom';
 
+///////////  Background Shapes ////////////
+import { overlappingCircles } from 'hero-patterns'
+
 /////// Check if element is in view //////////////////
-import InViewPort from './InViewPort';
+import { useInView } from 'react-intersection-observer';
 
 const Home = () => {
+  const dataLen = data.length;
+  const references = new Array(dataLen).fill('').map(_ => useInView({
+    triggerOnce: true,
+    // rootMargin: '-250px 0px',
+    // threshold: 1
+  })
+  )
+
+  useEffect(() => {
+    window.onbeforeunload = function () {
+      window.scrollTo(0, 0);
+    };
+  }, [])
 
   /////// Reference for arrow down icon //////////////////
   const scroll = useRef(null)
@@ -19,25 +35,6 @@ const Home = () => {
     scroll.current.scrollIntoView({ behavior: "smooth" })
   }
 
-  const dataLen = data.length;
-  const [reference, setreference] = useState([])
-  // const reference = useRef([])
-
-  // reference.current = Array(dataLen).fill('').map((_, idx) => reference.current[idx] = createRef())
-
-  useEffect(() => {
-    setreference(reference => (Array(dataLen).fill('').map((_, idx) => reference[idx] = createRef())
-    ))
-  }, [dataLen])
-
-  
-  const test = InViewPort(reference[0], "0px")
-  // console.log(reference)
-  // console.log(InViewPort(reference[0], "0px"))
-  // console.log(InViewPort(reference[1], "0px"))
-  // console.log(InViewPort(reference[5], "0px"))
-  // console.log(scroll)
-  
   return (
     <>
       <Box sx={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center', height: '95vh', backgroundImage: `url('/images/road.jpg')`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
@@ -60,12 +57,12 @@ const Home = () => {
           Functionalities
         </Typography>
       </Divider>
-      <Grid container spacing={5} justifyContent='space-around'>
+      <Grid container justifyContent='space-around' sx={{ background: overlappingCircles('#a2cf6e', .3), backgroundAttachment: 'fixed' }}>
         {
           data.map((info, idx) => (
-            <Slide in={true} direction="up" timeout={info.timeout} key={info.id}>
+            <Zoom in={references[idx][1]} timeout={1200} key={info.id}>
               <Grid item xs={12} md={6} >
-                <Box ref={reference[idx]} sx={{ minHeight: 700, display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'center', bgcolor: 'lightblue' }}>
+                <Box ref={references[idx][0]} sx={{ minHeight: 700, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', }}>
                   <Box>
                     <Typography variant='h5' align='center'>
                       {info.title}
@@ -74,76 +71,25 @@ const Home = () => {
                       {info.description}
                     </Typography>
                   </Box>
-                  <Box>
-                    <img src={info.image} />
-                  </Box>
+                  {info.image ? (
+                    <Box>
+                      <img src={info.image} height={530} />
+                    </Box>
+                  ) : (
+                    <Box>
+                      <Button color='secondary' variant='contained' component={Link} to='/signup'>
+                        {info.buttonTitle}
+                      </Button>
+                    </Box>
+                  )}
                 </Box>
               </Grid>
-            </Slide>
+            </Zoom>
           ))
         }
-        <Grid item xs={12} md={6}>
-          <Box sx={{
-            minHeight: 700, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', bgcolor: 'lightblue'
-          }}>
-            <Typography variant='h5' align='center'>
-              Sign Up
-            </Typography>
-            <Typography align='center'>
-              Are you ready to take your trip to the next level?
-            </Typography>
-            <Button variant='contained' component={Link} to='/signup'>
-              Sign Up
-            </Button>
-          </Box>
-        </Grid>
       </Grid >
-      {/* </Container> */}
     </>
   )
 }
 
 export default Home
-
-{/* <Grid item xs={12} md={6}>
-<Box sx={{ height: '75vh', bgcolor: 'orange' }}>
-  <Typography variant='h5' align='center'>
-    Add Events
-  </Typography>
-</Box>
-</Grid>
-<Grid item xs={12} md={6}>
-<Box sx={{ height: '50vh', bgcolor: 'yellow' }}>
-  <Typography variant='h5' align='center'>
-    Invite Friends
-  </Typography>
-</Box>
-</Grid>
-<Grid item xs={12} md={6}>
-<Box sx={{ height: '50vh', bgcolor: 'lightgreen' }}>
-  <Typography variant='h5' align='center'>
-    View Trip Map
-  </Typography>
-</Box>
-</Grid>
-<Grid item xs={12} md={6}>
-<Box sx={{ height: '50vh', bgcolor: 'green' }}>
-  <Typography variant='h5' align='center'>
-    Add Trip Expenses
-  </Typography>
-</Box>
-</Grid>
-<Grid item xs={12} md={6}>
-<Box sx={{ height: '50vh', bgcolor: 'silver' }}>
-  <Typography variant='h5' align='center'>
-    View Trip Calendar
-  </Typography>
-</Box>
-</Grid>
-<Grid item xs={12} md={6}>
-<Box sx={{ height: '50vh', bgcolor: 'green' }}>
-  <Typography variant='h5' align='center'>
-    Trip Chat Room
-  </Typography>
-</Box>
-</Grid> */}
