43d2cd883988c1699584952b270b41a3ec832d08
Corinne <83316799+nightsandwich@users.noreply.github.com>
2021-12-14 21:27:14 -0500

    homepage

--- client/components/Home/data.js
+++ client/components/Home/data.js
@@ -2,66 +2,66 @@ const data = [
     {
         id: 1,
         title: 'Group Trips and Outings',
-        description: 'Create and organize trips with friends and family',
+        description: 'Create and organize trips with friends.',
         image: 'images/tripsPage.png',
         timeout: Math.floor(Math.random() * 1500)
     },
     {
         id: 2,
-        title: 'Add Events',
+        title: 'Trip Events',
         description: 'Easily add, update and delete events in your trip.',
         image: 'images/tripEvents.png',
         timeout: Math.floor(Math.random() * 1500)
     },
     {
         id: 3,
-        title: 'Invite Friends and Family',
-        description: 'Sent friend and invite request to your friends and family.',
+        title: 'Trip Summary',
+        description: "View snapshots of a trip's latest activities.",
         image: 'images/inviteFriends.png',
         timeout: Math.floor(Math.random() * 1500)
     },
     {
         id: 4,
-        title: 'View Trips Map',
-        description: 'Take a look at your trips in the map.',
+        title: 'Map Features',
+        description: 'View your trips and events on the map and see where your friends are.',
         image: 'images/viewMap.png',
         timeout: Math.floor(Math.random() * 1500)
     },
     {
         id: 5,
-        title: 'Split Expenses',
-        description: 'Add trip expenses to easy split calculations.',
-        image: 'images/tripExpenses.png',
+        title: 'Calendar Features',
+        description: 'View your trips and events on the calendar.',
+        image: 'images/viewCalendar.png',
         timeout: Math.floor(Math.random() * 1500)
     },
     {
         id: 6,
-        title: 'View Trips Calendar',
-        description: 'Take a look at your trip calendar to organize your events.',
-        image: 'images/viewCalendar.png',
+        title: 'Trip Expenses',
+        description: 'Keep track of your spending and easily split the expenses among your friends.',
+        image: 'images/tripExpenses.png',
         timeout: Math.floor(Math.random() * 1500)
     },
     {
         id: 7,
         title: 'Trip Chat',
-        description: 'Chat with your friends and family that are part of the trip.',
+        description: 'Chat with your friends in the trip.',
         image: 'images/tripChat.png',
         timeout: Math.floor(Math.random() * 1500)
     },
     {
         id: 8,
         title: 'Notifications',
-        description: 'Receive friends request or trip invites notifications.',
+        description: 'View new friend requests, trip invitations, and money owed from past trips.',
         image: 'images/notifications.png',
         timeout: Math.floor(Math.random() * 1500)
     },
-    {
-        id: 9,
-        title: 'Manage Friends',
-        description: 'Send friend requests or remove friends from your contact list.',
-        image: 'images/manageFriends.png',
-        timeout: Math.floor(Math.random() * 1500)
-    },
+    // {
+    //     id: 9,
+    //     title: 'Manage Friends',
+    //     description: 'Send friend requests or remove friends from your contact list.',
+    //     image: 'images/manageFriends.png',
+    //     timeout: Math.floor(Math.random() * 1500)
+    // },
     // {
     //     id: 10,
     //     title: 'Sign Up',
